9df1340a737b0b0d661ea322f70867603cfd7731
Cayofds <cayoferreiradesouza@gmail.com>
2025-09-27 12:30:55 -0300

    Correção no redirecionamento para Home

--- script/script.js
+++ script/script.js
@@ -1,10 +1,3 @@
-document.getElementById("form-log").addEventListener("submit", function(event) {
-    event.preventDefault(); // Impede envio padrão
-    const nome = document.getElementById("nome").value;
-    // Redireciona para outra página com o valor como parâmetro
-    window.location.href = `resultado.html?nome=${encodeURIComponent(nome)}`;
-});
-
 function selecionarImgProduto(imgMiniaturaEscolhida) {
     document.getElementById("imgPrincipal").src = imgMiniaturaEscolhida.src;
 }
